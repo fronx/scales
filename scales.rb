67749f3e0350a2fdafbccca348360dbe6217083c
François Wurmus <fronx@wurmus.de>
2009-01-22 23:28:08 +0100

    ooh-oo child things'll get brighter...

--- scales.rb
+++ scales.rb
@@ -1,8 +1,8 @@
 #! /usr/bin/env ruby
 class Scale
-  TONES = %w(c c# d d# e f f# g g# a a# b)
-  STEPS = [2, 2, 1, 2, 2, 2, 1]
-  MAJMIN = [:major, :minor, :minor, :major, :major, :minor, :diminished]
+  TONES = %w(c c# d d# e f f# g g# a a# b)*2
+  STEPS = [2, 2, 1, 2, 2, 2, 1]*2
+  MAJMIN = [:major, :minor, :minor, :major, :major, :minor, :diminished]*2
   
   class << self
     TONES.each do |tone|
@@ -16,14 +16,21 @@ class Scale
   
   def initialize(tonic)
     raise "bad tonic: #{tonic.downcase}" unless TONES.include?(tonic.downcase)
-    @tonic = tonic.downcase
-    @offset = TONES.index(@tonic)
+    @tonic = tonic
+    @offset = TONES.index(@tonic.downcase)
   end
   
-  def self.all(start_with = 'c')
+  def self.all(start_with = 'C')
     start = TONES.index(start_with.downcase)
-    (TONES + TONES)[start..(start + 11)].map do |tonic|
-      self.new(tonic)
+    tonality = Scale.new(start_with).tonality
+    TONES[start..(start + 11)].map do |tonic|
+      self.new(tonality == :major ? tonic.upcase : tonic.downcase )
+    end
+  end
+
+  def self.expand(basis = 'C')
+    Scale.new(basis).tones.map do |tonic|
+      self.new(tonic) rescue nil
     end
   end
   
@@ -31,7 +38,7 @@ class Scale
   def subdominant; tone(4); end
   def dominant; tone(5); end
 
-  def tones(range = 1..8)
+  def tones(range = 1..7)
     range.map do |pos|
       tone(pos)
     end
@@ -41,7 +48,11 @@ class Scale
     sum = 0
     pos = bound_pos(pos)
     (2..pos).each do |i|
-      sum += STEPS[i - 2]
+      if tonality == :major
+        sum += STEPS[i - 2]
+      else
+        sum += STEPS[i + 4]
+      end
     end
     sum
   end
@@ -61,30 +72,59 @@ class Scale
   def tone(pos)
     pos = bound_pos(pos)
     tone = TONES[pos_to_index(pos)]
-    major?(pos) ? tone.upcase : tone.downcase
+    with_case(tone, pos)
   end
   
-  def major?(pos)
-    MAJMIN[pos - 1] == :major
+  def with_case(tone, pos)
+    look_pos = tonality == :major ? pos - 1 : pos + 4
+    if MAJMIN[look_pos] == :major
+      tone.upcase
+    elsif MAJMIN[look_pos] == :diminished
+      tone.upcase + '0'
+    else
+      tone.downcase
+    end
+  end
+  
+  def tonality
+    @tonic.upcase == @tonic ? :major : :minor
   end
 
-  def minor?(pos)
-    MAJMIN[pos - 1] == :minor
-  end
-  
   def pos(tone)
     tones.index(tone) + 1 rescue nil
   end
   
-  def to_s(range = 1..8)
+  def to_s(range = 1..7)
     tones(range).map do |tone|
-      tone.ljust(2)
+      tone.ljust(3)
     end.join('  ')
+  end
+  
+  def matrix
+    tones.map do |tonic|
+      Scale.new(tonic).tones rescue nil
+    end
   end
 end
 
-indent = 0
-Scale.all(ENV['start'] || 'E').each do |scale|
-  puts ' ' * indent + scale.to_s
-  indent += 2
+
+def print_with_indentation(scales)
+  indent = 0
+  scales.each do |scale|
+    puts ' ' * indent + scale.to_s if scale
+    indent += 5
+  end
 end
+
+class String
+  def underlined(char = '=')
+    self + "\n" + char * self.length
+  end
+end
+
+start = $*.first || 'E'
+puts "Expanded #{start} scale:".underlined
+print_with_indentation(Scale.expand(start))
+puts 
+puts "All #{Scale.new(start).tonality} scales starting with #{start}:".underlined
+print_with_indentation(Scale.all(start))
